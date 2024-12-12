526b7c87df2c5afafd476d64d09ab4b3e3f0c4ea
Alex Neves <3845319+alexjneves@users.noreply.github.com>
2024-12-12 22:22:30 +0000

    Day 5 part 2

--- src/day5/day5.rs
+++ src/day5/day5.rs
@@ -24,16 +24,22 @@ impl Day for Day5 {
 fn part1(input: &Vec<String>) -> i32 {
     let (rules, pages) = parse_input(input);
 
-    println!("{:?}", rules);
-    println!("{:?}", pages);
-
-    get_valid_pages(&rules, &pages).iter()
+    let (valid_pages, _) = split_pages(&rules, &pages);
+        
+    valid_pages.iter()
         .map(|page| get_middle_page_number(page))
         .sum()
 }
 
 fn part2(input: &Vec<String>) -> i32 {
-    0
+    let (rules, pages) = parse_input(input);
+
+    let (_, invalid_pages) = split_pages(&rules, &pages);
+        
+    invalid_pages.iter()
+        .map(|page| fix_page(&rules, &page))
+        .map(|page| get_middle_page_number(&page))
+        .sum()
 }
 
 fn parse_input(input: &Vec<String>) -> (Vec<PageOrderingRule>, Vec<Vec<i32>>) {
@@ -76,45 +82,75 @@ fn parse_page(input: &String) -> Vec<i32> {
         .collect()
 }
 
-fn get_valid_pages<'a>(rules: &Vec<PageOrderingRule>, pages: &'a Vec<Vec<i32>>) -> Vec<&'a Vec<i32>> {
+fn split_pages<'a>(rules: &Vec<PageOrderingRule>, pages: &'a Vec<Vec<i32>>) -> (Vec<&'a Vec<i32>>, Vec<&'a Vec<i32>>) {
     let mut valid_pages: Vec<&Vec<i32>> = vec![];
+    let mut invalid_pages: Vec<&Vec<i32>> = vec![];
 
     for page in pages {
         let mut is_valid = true;
 
         for rule in rules {
-            if let Some(first_index) = page.iter().position(|i| *i == rule.first) {
-                if let Some(second_index) = page.iter().position(|i| *i == rule.second) {
-                    if first_index > second_index {
-                        is_valid = false;
-                        break;
-                    }
-                }
+            if !does_page_satisfy_rule(rule, page) {
+                is_valid = false;
+                break;
             }
         }
 
         if is_valid {
             valid_pages.push(page);
+        } else {
+            invalid_pages.push(page);
         }
     }
 
-    valid_pages
+    (valid_pages, invalid_pages)
 }
 
-// fn get_middle_page_number<'a>(pages: &'a Vec<Vec<i32>>) -> Vec<i32> {
-//     let mut middle_numbers: Vec<i32> = vec![];
+fn does_page_satisfy_rule(rule: &PageOrderingRule, page: &Vec<i32>) -> bool {
+    if let Some(first_index) = page.iter().position(|i| *i == rule.first) {
+        if let Some(second_index) = page.iter().position(|i| *i == rule.second) {
+            return first_index < second_index;
+        }
+    }
 
-//     for page in pages {
-//         let middle_index = page.len() / 2;
-//         middle_numbers.push(page.get(middle_index).unwrap().to_owned());
-//     }
-
-//     middle_numbers
-// }
+    return true;
+}
 
 fn get_middle_page_number(page: &Vec<i32>) -> i32 {
     let middle_index = page.len() / 2;
     page.get(middle_index).unwrap().to_owned()
+}
+
+fn fix_page(rules: &Vec<PageOrderingRule>, page: &Vec<i32>) -> Vec<i32> {
+    let mut result = page.to_owned();
+    let mut is_valid = false;
+
+    while !is_valid {
+        for rule in rules {
+            fix_page_for_rule(rule, &mut result);
+        }
+
+        is_valid = true;
+
+        for rule in rules {
+            if !does_page_satisfy_rule(rule, &result) {
+                is_valid = false;
+                break;
+            }
+        }
+    }
+
+    result
+}
+
+fn fix_page_for_rule(rule: &PageOrderingRule, page: &mut Vec<i32>) -> () {
+    if let Some(first_index) = page.iter().position(|i| *i == rule.first) {
+        if let Some(second_index) = page.iter().position(|i| *i == rule.second) {
+            if first_index > second_index {
+                page.swap(first_index, second_index);
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -143,7 +179,7 @@ mod tests {
 
     #[test]
     fn day5_part2_example_input() {
-        const EXPECTED_ANSWER: i32 = 0;
+        const EXPECTED_ANSWER: i32 = 123;
 
         let day5 = Day5 {};
         let answer = day5.run(Part::Two, InputType::Example);
@@ -153,7 +189,7 @@ mod tests {
 
     #[test]
     fn day5_part2_custom_input() {
-        const EXPECTED_ANSWER: i32 = 0;
+        const EXPECTED_ANSWER: i32 = 4145;
 
         let day5 = Day5 {};
         let answer = day5.run(Part::Two,InputType::Custom);
